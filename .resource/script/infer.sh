1eef601ad01823ce5dcc29d120f3692c5bfff9bc
Agustín <27942554+agustin-golmar@users.noreply.github.com>
2019-08-18 17:10:19 -0300

    Remove sym-link.

--- .resource/script/infer.sh
+++ .resource/script/infer.sh
@@ -2,7 +2,6 @@
 
 set -ev
 
-VERSION=0.17.0; \
-curl -sSL "https://github.com/facebook/infer/releases/download/v$VERSION/infer-linux64-v$VERSION.tar.xz" \
-| sudo tar -C /opt -xJ && \
-ln -s "/opt/infer-linux64-v$VERSION/bin/infer" /usr/local/bin/infer
+INFER_VERSION=0.17.0; \
+curl -sSL "https://github.com/facebook/infer/releases/download/v$INFER_VERSION/infer-linux64-v$INFER_VERSION.tar.xz" \
+| sudo tar -C /opt -xJ && ls -la
