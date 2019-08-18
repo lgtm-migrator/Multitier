7847e6416f3dde4c7723ec01557259a9eb5321ef
Agustín <27942554+agustin-golmar@users.noreply.github.com>
2019-08-18 17:06:38 -0300

    Avoid output from curl.

--- .resource/script/infer.sh
+++ .resource/script/infer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -e
+set -ev
 
-VERSION=0.17.0;
-curl -sSL "https://github.com/facebook/infer/releases/download/v$VERSION/infer-linux64-v$VERSION.tar.xz"
-sudo tar -C /opt -xJ
+VERSION=0.17.0; \
+curl -sSL "https://github.com/facebook/infer/releases/download/v$VERSION/infer-linux64-v$VERSION.tar.xz" \
+| sudo tar -C /opt -xJ && \
 ln -s "/opt/infer-linux64-v$VERSION/bin/infer" /usr/local/bin/infer
