25e2164094dace807868860c498beede9031dc86
Agustín <27942554+agustin-golmar@users.noreply.github.com>
2019-10-18 18:25:49 -0300

    Triggering Drone. Better script for Infer.

--- .resource/script/infer.sh
+++ .resource/script/infer.sh
@@ -3,5 +3,6 @@
 set -ev
 
 INFER_VERSION=0.17.0; \
+export INFER_HOME=/infer/infer-linux64-v$INFER_VERSION/bin; \
 curl -sSL "https://github.com/facebook/infer/releases/download/v$INFER_VERSION/infer-linux64-v$INFER_VERSION.tar.xz" \
-| sudo tar -C /opt -xJ && ls -l /opt
+| sudo tar -C /infer -xJ && ls -l /infer
