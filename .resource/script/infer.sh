02535932e7cc6dd7372a487feae8c3bb89ab8a7d
Agustín <27942554+agustin-golmar@users.noreply.github.com>
2019-10-18 19:06:42 -0300

    Fix the path.

--- .resource/script/infer.sh
+++ .resource/script/infer.sh
@@ -6,6 +6,6 @@ INFER_VERSION=0.17.0; \
 export INFER_HOME=/opt/infer-linux64-v$INFER_VERSION/bin; \
 curl -sSL "https://github.com/facebook/infer/releases/download/v$INFER_VERSION/infer-linux64-v$INFER_VERSION.tar.xz" \
 | sudo tar -C /opt -xJ; \
-ls -l /opt/infer; \
+ls -l /opt; \
 sudo apt-get install python2.7-minimal; \
 python2.7 --version;
