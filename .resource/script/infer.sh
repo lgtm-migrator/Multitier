90a0754c353fe8fecb9df00a09bd2f22b5b9f936
Agustín <27942554+agustin-golmar@users.noreply.github.com>
2019-10-18 19:02:56 -0300

    Path for Infer.

--- .resource/script/infer.sh
+++ .resource/script/infer.sh
@@ -3,9 +3,9 @@
 set -ev
 
 INFER_VERSION=0.17.0; \
-export INFER_HOME=/opt/infer/infer-linux64-v$INFER_VERSION/bin; \
+export INFER_HOME=/opt/infer-linux64-v$INFER_VERSION/bin; \
 curl -sSL "https://github.com/facebook/infer/releases/download/v$INFER_VERSION/infer-linux64-v$INFER_VERSION.tar.xz" \
-| sudo tar -C /opt/infer -xJ; \
+| sudo tar -C /opt -xJ; \
 ls -l /opt/infer; \
 sudo apt-get install python2.7-minimal; \
 python2.7 --version;
