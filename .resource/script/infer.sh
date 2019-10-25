df67e980525dd282058c00c34792b64ba61fb33d
Agustín <agustin.golmar@gmail.com>
2019-10-25 17:34:12 -0300

    More variables. Conditional mkdir.

--- .resource/script/infer.sh
+++ .resource/script/infer.sh
@@ -2,12 +2,24 @@
 
 set -ev
 
+# Constants:
+INFER_HOME="${HOME}/infer"
+INFER_URL="https://github.com/facebook/infer/releases/download/v${INFER_VERSION}/infer-linux64-v${INFER_VERSION}.tar.xz"
+
 # Make a folder for Infer:
-mkdir ${HOME}/infer
+ls -la ${HOME}
+ls -la $INFER_HOME
+mkdir -p $INFER_HOME
 
 # Download last Infer:
-curl -sSL "https://github.com/facebook/infer/releases/download/v${INFER_VERSION}/infer-linux64-v${INFER_VERSION}.tar.xz" \
-	| sudo tar -C ${HOME}/infer -xJ
+curl -sSL $INFER_URL | tar -C $INFER_HOME -xJ
+
+# Move one level up:
+mv $INFER_HOME/infer-linux64-v$INFER_VERSION/* .
+rm -fr $INFER_HOME/infer-linux64-v$INFER_VERSION
+
+ls -la ${HOME}
+ls -la $INFER_HOME
 
 # Install Python v2.7 (required by Infer):
 sudo apt-get install python2.7-minimal
